e659e9acdcf94b2c5c56ce4a5d131a4c5d2b62e2
Ilyas Assylbekov <ilyas.asylbekov1305@gmail.com>
2023-09-23 18:12:58 +0600

    Fix display

--- videoAnalytics.py
+++ videoAnalytics.py
@@ -1,8 +1,10 @@
 import cv2
 import tensorflow as tf
+import numpy as np
+import keyboard
 
 # Load SSD MobileNet
-model = tf.saved_model.load("./ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8(1)/")
+model = tf.saved_model.load("ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved_model")
 
 # Camera capture
 cap = cv2.VideoCapture(0) #default TODO: download video test
@@ -11,19 +13,24 @@ while True:
     # Capture frame
     ret, frame = cap.read()
 
+    resized_frame = cv2.resize(frame, (640, 480))
+    input_tensor = np.expand_dims(resized_frame, axis=0)
+
     # Object detection
-    detections = model(frame)
+    detections = model(input_tensor)
 
     # Process detections
     num_people = len(detections)
 
     # TODO: store/analyze data
+    print(num_people)
 
     # Display
     cv2.imshow('Live', frame)
+    cv2.waitKey(1)
 
     # 'q' to exit
-    if cv2.waitkey(1) & 0xFF == ord('q'):
+    if keyboard.is_pressed("q"):
         break
 
 # Release camera
